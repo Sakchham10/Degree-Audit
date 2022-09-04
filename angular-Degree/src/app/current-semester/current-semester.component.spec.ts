c85acac7e611d5a8e5186955bc60ab3c8b91db35
Sakchham10 <Sakchham10@Hotmail.com>
2022-09-04 14:55:27 -0500

    Added server within the main file

---
+++ angular-Degree/src/app/current-semester/current-semester.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CurrentSemesterComponent } from './current-semester.component';
+
+describe('CurrentSemesterComponent', () => {
+  let component: CurrentSemesterComponent;
+  let fixture: ComponentFixture<CurrentSemesterComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CurrentSemesterComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(CurrentSemesterComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ angular-Degree/src/app/current-semester/current-semester.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-current-semester',
+  templateUrl: './current-semester.component.html',
+  styleUrls: ['./current-semester.component.scss']
+})
+export class CurrentSemesterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
